c898dbfd41b2d5004b330f1db12738ec0531151a
matthieu <matthieu.aussaguel@gmail.com>
2012-06-30 15:04:29 +1000

    Add show animation tests

--- spec/javascripts/MiniNotificationSpec.coffee
+++ spec/javascripts/MiniNotificationSpec.coffee
@@ -1,22 +1,89 @@
 describe 'miniNotification', ->
   options =
-    position: 'top'
+    position: 'bottom'
+    show: false
+
 
   beforeEach ->
     loadFixtures 'fragment.html'
-    @$element = $('#fixtures')
+    @$element = $('#fixtures #notification')
 
   it 'should be available on the jQuery object', ->
     expect($.fn.miniNotification).toBeDefined()
 
   it 'should be chainable', ->
-    expect(@$element.miniNotification(options)).toBe(@$element)
+    expect(@$element.miniNotification(options)).toBe @$element
 
-  it 'should offers default values', ->
+  it 'should offer default values', ->
     plugin = new $.miniNotification(@$element[0], options)
 
     expect(plugin.defaults).toBeDefined()
 
   it 'should overwrites the settings', ->
     plugin = new $.miniNotification(@$element[0], options)
-    expect(plugin.settings.position).toBe(options.position)
+
+    expect(plugin.settings.position).toBe options.position
+    expect(plugin.settings.show).toBe options.show
+
+  describe 'show', ->
+    spyOnShowAnimatewWithOptions = (options = {}) -> 
+      options = $.extend {show: false}, options
+      plugin = new $.miniNotification(@$element, options)
+      spyOn(plugin.$element, 'animate')
+      plugin.show()
+      plugin
+
+    describe 'position', ->
+
+      it 'should position the notification at the top of the page by default', ->
+        plugin = spyOnShowAnimatewWithOptions()
+        
+        expect(plugin.$element.animate).toHaveBeenCalledWith({top: 0, opacity: plugin.settings.opacity}, jasmine.any(Number), jasmine.any(String),  jasmine.any(Function))
+
+      it 'should position the notification at the bottom of the page if specified in the options', ->
+        plugin = spyOnShowAnimatewWithOptions({position: 'bottom'})
+        
+        expect(plugin.$element.animate).toHaveBeenCalledWith({bottom: 0, opacity: plugin.settings.opacity}, jasmine.any(Number), jasmine.any(String),  jasmine.any(Function))
+
+      it 'should position the notification at the top of the page if specified in the options', ->
+        plugin = spyOnShowAnimatewWithOptions({position: 'top'})
+                
+        expect(plugin.$element.animate).toHaveBeenCalledWith({top: 0, opacity: jasmine.any(Number)}, jasmine.any(Number), jasmine.any(String),  jasmine.any(Function))
+
+      it 'should position the notification at the top of the page if option is invalid', ->
+        plugin = spyOnShowAnimatewWithOptions({position: 'invalid'})
+        
+        expect(plugin.$element.animate).toHaveBeenCalledWith({top: 0, opacity: jasmine.any(Number)}, jasmine.any(Number), jasmine.any(String),  jasmine.any(Function))
+
+    describe 'opacity', ->
+      it 'should animate the element to the default opacity if not specified', ->
+        plugin = spyOnShowAnimatewWithOptions()
+
+        expect(plugin.$element.animate).toHaveBeenCalledWith({opacity: plugin.settings.opacity, top: jasmine.any(Number)}, jasmine.any(Number), jasmine.any(String),  jasmine.any(Function))
+
+      it 'should animate the element to the opacity specified', ->
+        plugin = spyOnShowAnimatewWithOptions({opacity: 0.5})
+                
+        expect(plugin.$element.animate).toHaveBeenCalledWith({opacity: 0.5, top: jasmine.any(Number)}, jasmine.any(Number), jasmine.any(String),  jasmine.any(Function))
+
+    describe 'speed', ->
+      it 'should animate the element notification with default show speed', ->
+        plugin = spyOnShowAnimatewWithOptions()
+
+        expect(plugin.$element.animate).toHaveBeenCalledWith(jasmine.any(Object), plugin.settings.showSpeed, jasmine.any(String),  jasmine.any(Function))
+
+      it 'should animate the element notification with specified show speed', ->
+        plugin = spyOnShowAnimatewWithOptions({showSpeed: 2000})
+        
+        expect(plugin.$element.animate).toHaveBeenCalledWith(jasmine.any(Object), 2000, jasmine.any(String),  jasmine.any(Function))
+
+    describe 'easing', ->
+      it 'should animate the element notification with no easing equation by default', ->
+        plugin = spyOnShowAnimatewWithOptions()
+        
+        expect(plugin.$element.animate).toHaveBeenCalledWith(jasmine.any(Object), jasmine.any(Number), '',  jasmine.any(Function))
+
+      it 'should animate the element notification with specified easing equation', ->
+        plugin = spyOnShowAnimatewWithOptions({showEasing: 'swing'})
+        
+        expect(plugin.$element.animate).toHaveBeenCalledWith(jasmine.any(Object), jasmine.any(Number), 'swing',  jasmine.any(Function))
--- spec/javascripts/MiniNotificationSpec.js
+++ spec/javascripts/MiniNotificationSpec.js
@@ -3,11 +3,12 @@
   describe('miniNotification', function() {
     var options;
     options = {
-      position: 'top'
+      position: 'bottom',
+      show: false
     };
     beforeEach(function() {
       loadFixtures('fragment.html');
-      return this.$element = $('#fixtures');
+      return this.$element = $('#fixtures #notification');
     });
     it('should be available on the jQuery object', function() {
       return expect($.fn.miniNotification).toBeDefined();
@@ -15,15 +16,120 @@
     it('should be chainable', function() {
       return expect(this.$element.miniNotification(options)).toBe(this.$element);
     });
-    it('should offers default values', function() {
+    it('should offer default values', function() {
       var plugin;
       plugin = new $.miniNotification(this.$element[0], options);
       return expect(plugin.defaults).toBeDefined();
     });
-    return it('should overwrites the settings', function() {
+    it('should overwrites the settings', function() {
       var plugin;
       plugin = new $.miniNotification(this.$element[0], options);
-      return expect(plugin.settings.position).toBe(options.position);
+      expect(plugin.settings.position).toBe(options.position);
+      return expect(plugin.settings.show).toBe(options.show);
+    });
+    return describe('show', function() {
+      var spyOnShowAnimatewWithOptions;
+      spyOnShowAnimatewWithOptions = function(options) {
+        var plugin;
+        if (options == null) {
+          options = {};
+        }
+        options = $.extend({
+          show: false
+        }, options);
+        plugin = new $.miniNotification(this.$element, options);
+        spyOn(plugin.$element, 'animate');
+        plugin.show();
+        return plugin;
+      };
+      describe('position', function() {
+        it('should position the notification at the top of the page by default', function() {
+          var plugin;
+          plugin = spyOnShowAnimatewWithOptions();
+          return expect(plugin.$element.animate).toHaveBeenCalledWith({
+            top: 0,
+            opacity: plugin.settings.opacity
+          }, jasmine.any(Number), jasmine.any(String), jasmine.any(Function));
+        });
+        it('should position the notification at the bottom of the page if specified in the options', function() {
+          var plugin;
+          plugin = spyOnShowAnimatewWithOptions({
+            position: 'bottom'
+          });
+          return expect(plugin.$element.animate).toHaveBeenCalledWith({
+            bottom: 0,
+            opacity: plugin.settings.opacity
+          }, jasmine.any(Number), jasmine.any(String), jasmine.any(Function));
+        });
+        it('should position the notification at the top of the page if specified in the options', function() {
+          var plugin;
+          plugin = spyOnShowAnimatewWithOptions({
+            position: 'top'
+          });
+          return expect(plugin.$element.animate).toHaveBeenCalledWith({
+            top: 0,
+            opacity: jasmine.any(Number)
+          }, jasmine.any(Number), jasmine.any(String), jasmine.any(Function));
+        });
+        return it('should position the notification at the top of the page if option is invalid', function() {
+          var plugin;
+          plugin = spyOnShowAnimatewWithOptions({
+            position: 'invalid'
+          });
+          return expect(plugin.$element.animate).toHaveBeenCalledWith({
+            top: 0,
+            opacity: jasmine.any(Number)
+          }, jasmine.any(Number), jasmine.any(String), jasmine.any(Function));
+        });
+      });
+      describe('opacity', function() {
+        it('should animate the element to the default opacity if not specified', function() {
+          var plugin;
+          plugin = spyOnShowAnimatewWithOptions();
+          return expect(plugin.$element.animate).toHaveBeenCalledWith({
+            opacity: plugin.settings.opacity,
+            top: jasmine.any(Number)
+          }, jasmine.any(Number), jasmine.any(String), jasmine.any(Function));
+        });
+        return it('should animate the element to the opacity specified', function() {
+          var plugin;
+          plugin = spyOnShowAnimatewWithOptions({
+            opacity: 0.5
+          });
+          return expect(plugin.$element.animate).toHaveBeenCalledWith({
+            opacity: 0.5,
+            top: jasmine.any(Number)
+          }, jasmine.any(Number), jasmine.any(String), jasmine.any(Function));
+        });
+      });
+      describe('speed', function() {
+        it('should animate the element notification with default show speed', function() {
+          var plugin;
+          plugin = spyOnShowAnimatewWithOptions();
+          return expect(plugin.$element.animate).toHaveBeenCalledWith(jasmine.any(Object), plugin.settings.showSpeed, jasmine.any(String), jasmine.any(Function));
+        });
+        return it('should animate the element notification with specified show speed', function() {
+          var plugin;
+          plugin = spyOnShowAnimatewWithOptions({
+            showSpeed: 2000
+          });
+          return expect(plugin.$element.animate).toHaveBeenCalledWith(jasmine.any(Object), 2000, jasmine.any(String), jasmine.any(Function));
+        });
+      });
+      return describe('easing', function() {
+        it('should animate the element notification with no easing equation by default', function() {
+          var plugin;
+          plugin = spyOnShowAnimatewWithOptions();
+          return expect(plugin.$element.animate).toHaveBeenCalledWith(jasmine.any(Object), jasmine.any(Number), '', jasmine.any(Function));
+        });
+        return it('should animate the element notification with specified easing equation', function() {
+          var plugin;
+          plugin = spyOnShowAnimatewWithOptions({
+            showEasing: 'swing'
+          });
+          return expect(plugin.$element.animate).toHaveBeenCalledWith(jasmine.any(Object), jasmine.any(Number), 'swing', jasmine.any(Function));
+        });
+      });
     });
   });
 
